3fce19df26c6d8185e5ea8105c4dc7c4222c126e
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 17:21:27 -0300

    routes videogames created

--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -27,6 +27,18 @@ router.get("/videogames", async (req, res, next) => {
   }
 });
 
+router.get("/videogame/:videogame_id", async (req, res, next) => {
+  const { videogame_id } = req.params;
+  try {
+    let videogame = await Videogame.findOne({
+      where: { videogame_id: videogame_id },
+      include: [{ model: Genre }, { model: Platform }],
+    });
+    res.json(videogame)
+  } catch (err) {
+    next(err);
+  }
+});
 router.post("/videogame", async (req, res, next) => {
   try {
     const {
@@ -51,6 +63,52 @@ router.post("/videogame", async (req, res, next) => {
     res.json(newVideogame);
   } catch (err) {
     next(err);
+  }
+});
+
+router.put("/videogame/:videogame_id", async (req, res, next) => {
+  const {
+    videogame_name,
+    videogame_description,
+    videogame_release_date,
+    videogame_rating,
+  } = req.body;
+  const { videogame_id } = req.params;
+  try {
+    const editVideogame = await Videogame.findOne({
+      where: { videogame_id: videogame_id },
+    });
+    if (editVideogame) {
+      editVideogame.videogame_name = videogame_name;
+      editVideogame.videogame_description = videogame_description;
+      editVideogame.videogame_release_date = videogame_release_date;
+      editVideogame.videogame_rating = videogame_rating;
+      await editVideogame.save();
+      res.json(editVideogame);
+    } else {
+      res.json({
+        message: `ID received: ${videogame_id} not exists in DB`,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/videogame/:videogame_id", async (req, res, next) => {
+  const { videogame_id } = req.params;
+  const videogame = await Videogame.findOne({
+    where: { videogame_id: videogame_id },
+  });
+  if (videogame) {
+    videogame
+      .destroy()
+      .then(() =>
+        res.status(200).json({ message: `videogame succefully deleted` })
+      )
+      .catch((err) => next(err));
+  } else {
+    res.status(404).json({ message: "videogame nox exists" });
   }
 });
 module.exports = router;
