714234df4a9ec727f5331c934bd80778bef76a96
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 14:42:34 -0300

    model genre created

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -2,5 +2,14 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("genre", {
+      genre_id:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
 
+      },
+      genre_name:{
+        type: DataTypes.TEXT,
+        allowNull: false
+      }
   })}
