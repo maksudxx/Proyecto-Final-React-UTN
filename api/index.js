5adcfbce1b9072a6fc2a12a90d5391e38eb21d9d
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 15:43:36 -0300

    inyected data

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const server = require("./src/app");
-const { conn } = require('./src/db.js');
+const { conn, Genre, Platform, Videogame} = require('./src/db.js');
+const {createData} = require('./preloadData')
 require("dotenv").config();
 
 conn.sync({ force: false }).then(() => {
-    server.listen(3001, () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
+    server.listen(process.env.PORT || 3001, async () => {
+      console.log('%s listening at 3001'); 
+      const data = await createData();
+      const genre = await Genre.bulkCreate(data.genre);
+      console.log('*********Genres created');
+      const platform = await Platform.bulkCreate(data.platform);
+      console.log('*********Platforms created');
+      const video = await Videogame.bulkCreate(data.videogame);
+      await video[0].addGenre([genre[0].genre_id, genre[1].genre_id])
+      await video[0].addPlatform([platform[0].platform_id, platform[1].platform_id, platform[2].platform_id]), 
+      console.log('*********Videogames created')
     });
   });
