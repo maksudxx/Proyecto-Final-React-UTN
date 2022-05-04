f206f65b3221494fe2a7310bd8d274d7a8ad7a6e
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 14:38:49 -0300

    model platform created

--- api/src/models/Platform.js
+++ api/src/models/Platform.js
@@ -2,5 +2,13 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("platform", {
-        
+        platform_id:{
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true
+        },
+        platform_name:{
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
   })}
