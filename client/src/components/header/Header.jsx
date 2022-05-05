08e2c0e00f917dde72605862846b925842d0157f
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 23:10:43 -0300

    header and navbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import LOGO from "../../assets/logo.jpg";
+import LOGO from "../../assets/logo.gif";
 import { Navbar } from "../navbar/Navbar";
+import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <h2>Videogames APP</h2>
-        <img src={LOGO} alt="logo" />
+    <header className={styles.container}>
+      <div className={styles.containerTitle}>
+        <h2 className={styles.title}>Videogames APP</h2>
+        <img src={LOGO} alt="logo" className={styles.image}/>
       </div>
       <Navbar/>
     </header>
