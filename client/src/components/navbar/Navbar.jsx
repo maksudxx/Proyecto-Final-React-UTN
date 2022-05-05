bb2bbfe3ae4e11c19892cd11c025c5f934f152bd
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-04 22:36:04 -0300

    navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 export const Navbar = () => {
   return (
-    <ul>
-        <li></li>
-    </ul>
+    <div className={styles.container}>
+        <Link to='/dashboard' className={styles.link}>Lista de juegos</Link>
+        <Link to='/newGame' className={styles.link}>Agregar un juego</Link>
+        <Link to='/about' className={styles.link}>Acerca de</Link>
+        <Link to='/login' className={styles.link}>Login</Link>
+    </div>
   )
 }
