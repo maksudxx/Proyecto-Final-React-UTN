3a7cb96cb96960f4307754a9104d709e6b24f658
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-05 13:50:42 -0300

    .

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+let cors = require("cors");
 require("./db.js");
 
 const server = express();
@@ -13,9 +14,10 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+server.use(cors());
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
