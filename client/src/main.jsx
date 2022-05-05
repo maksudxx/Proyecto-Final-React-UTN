7c955271fca667ad48b746d2030ac64f0eeab3f8
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-05 13:51:17 -0300

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+// import {Provider} from "react-redux"
 import "./index.css";
+// import store from './redux/store/index'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+
 );
