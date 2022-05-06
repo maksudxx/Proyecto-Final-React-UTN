1c8a12643a0e0caef206f3ab471efd4530261327
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-06 09:10:39 -0300

    types created

--- client/src/redux/types/index.js
+++ client/src/redux/types/index.js
@@ -1,2 +1,12 @@
 //videogames
 export const GET_VIDEOGAMES = "GET_VIDEOGAMES"
+export const GET_VIDEOGAMES_ID = "GET_VIDEOGAMES_ID"
+export const PUT_VIDEOGAME = "PUT_VIDEOGAME"
+export const DELETE_VIDEOGAME = "DELETE_VIDEOGAME"
+export const CREATE_VIDEOGAME = "CREATE_VIDEOGAME"
+
+//Genres
+export const GET_GENRES = "GET_GENRES"
+
+//Platforms
+export const GET_PLATFORMS = "GET_PLATFORMs"
