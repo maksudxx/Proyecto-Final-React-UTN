21da99c5c919a630531d9281b8be24b8c9dcdaa1
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-06 17:31:25 -0300

    navbar fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
+import Videogames from "./pages/videogames/Videogames";
 import Landing from "./pages/landing/Landing";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/*" element={<Header />} />
+        <Route path="/*" element={<Header />}>
+          <Route path="videogames" element={<Videogames />} />{" "}
+        </Route>
       </Routes>
     </div>
   );
