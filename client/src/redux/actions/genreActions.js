90d2964f8cbc419a671334ea00bba071fb3b83f5
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-06 09:14:34 -0300

    actions genre created

---
+++ client/src/redux/actions/genreActions.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { GET_GENRES } from "../types";
+
+export function getVideogames() {
+    return async function (dispatch) {
+      const response = await axios.get("http://localhost:3001/genres");
+      const json = response.data;
+      dispatch({ type: GET_GENRES, payload: json });
+    };
+  }
