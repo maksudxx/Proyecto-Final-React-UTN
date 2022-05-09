21a16873ee05987a84006b65c63e697ac62c783d
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-08 23:19:32 -0300

    styles cards

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -15,31 +15,30 @@ const Card = ({ id, name, image, rating, genres, platforms, release }) => {
     <li className={styles.containerCard}>
       <img src={image} alt={name} className={styles.image} />
       <div className={styles.containerInformation}>
-        <div className={styles.platforms}>
-          <div className={styles.containerInfo}>
-            {platforms?.map((p) => (
-              <p className={styles.namePlatform}>{p.platform_name}</p>
-            ))}
-          </div>
-        </div>
         <Link to={`/videogames/${id}`} className={styles.link}>
           <h3 className={styles.titleGame}>{name}</h3>
         </Link>
 
         <div className={styles.info}>
-          <p>Fecha de Lanzamiento: </p>
+          <p  className={styles.pagraph}>Fecha de Lanzamiento: </p>
           <p>{formatDate(date)}</p>
         </div>
         <div className={styles.info}>
-          <p>Generos:</p>
+          <p className={styles.pagraph}>Generos:</p>
           <div className={styles.info}>
             {genres?.map((g) => (
               <p className={styles.namePlatform}>{g.genre_name}</p>
             ))}
           </div>
         </div>
+        <div className={`${styles.info} ${styles.wrap}`}>
+          <p  className={styles.pagraph}>Plataformas: </p>
+          {platforms?.map((p) => (
+            <p className={styles.namePlatform}>{p.platform_name}</p>
+          ))}
+        </div>
         <div className={`${styles.info} ${styles.end}`}>
-          <p>Calificacion: </p>
+          <p  className={styles.pagraph}>Calificacion: </p>
           <div className={styles.calification}>
             <AiOutlineStar className={styles.star} />
             <p>{rating}</p>
