3b3b4e8c7e9a62335ac860ce2ac774be167d1634
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-11 10:03:50 -0300

    styles header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import LOGO from "../../assets/joystick.png";
 import { Navbar } from "../navbar/Navbar";
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.containerTitle}>
-       
-        <img src={LOGO} alt="logo" className={styles.image}/>
+        <img src={LOGO} alt="logo" className={styles.image} />
         <h3 className={styles.title}>Videogames API</h3>
       </div>
-      <Navbar/>
-      
+      <Navbar />
     </header>
   );
 };
