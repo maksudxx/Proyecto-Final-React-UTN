e18981593d6923fb9b7394f8d478cc24f1c07d64
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-12 12:18:28 -0300

    view newVideogame created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,22 @@ import Videogames from "./pages/videogames/Videogames";
 import Landing from "./pages/landing/Landing";
 import Header from "./components/header/Header";
 import VideogameDetails from "./pages/videogameDetails/VideogameDetails";
-
+import NewVideogame from "./pages/newVideogame/NewVideogame";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Landing}/>
-        <Route path='/'>
-          <Header/>
-          <Route exact path='/videogames' component={Videogames}/>
-          <Route exact path='/videogames/:videogame_id' component={VideogameDetails}/>
+        <Route exact path="/" component={Landing} />
+        <Route path="/">
+          <Header />
+          <Route exact path="/videogames" component={Videogames} />
+          <Route
+            exact
+            path="/videogames/:videogame_id"
+            component={VideogameDetails}
+          />
+          <Route exact path="/newGame" component={NewVideogame} />
         </Route>
       </Switch>
     </div>
---
+++ client/src/pages/newVideogame/NewVideogame.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+import { getGenres } from "../../redux/actions/genreActions";
+import { getPlatforms } from "../../redux/actions/platformAction";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./NewVideogame.module.css";
+
+const NewVideogame = () => {
+  const [input, setInput] = useState({
+    videogame_name: "",
+    videogame_description: "",
+    videogame_release_date: "",
+    videogame_rating: 0,
+    videogame_image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdetallesorballo.com%2Fimagen-de-prueba-320x240%2F&psig=AOvVaw2dixc9donc4t_KJbwymqxt&ust=1652361427036000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJD14ZLE1_cCFQAAAAAdAAAAABAD",
+    genres: [],
+    platform_id: [],
+  });
+
+  const dispatch = useDispatch();
+  const genres = useSelector((state) => state.genre.genres);
+  const platforms = useSelector((state) => state.platform.platforms);
+
+  useEffect(() => {
+    dispatch(getGenres());
+    dispatch(getPlatforms());
+  }, [dispatch]);
+
+  const handleInputChange = function (e) {
+    e.preventDefault();
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  let arrayGenres = [];
+  const handleChangeGenre = (e) => {
+    arrayGenres.push(e.target.value);
+    console.log(arrayGenres);
+  };
+
+  const arrayPlatforms = [];
+  const handleChangePlatform = (e) => {
+    arrayPlatforms.push(e.target.value);
+    console.log(arrayPlatforms);
+  };
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>PUBLICAR UN NUEVO JUEGO</p>
+      <form className={styles.form}>
+        <div className={styles.containerInput}>
+          <p>Nombre del juego:</p>
+          <input
+            type="text"
+            name="videogame_name"
+            value={input.videogame_name}
+            placeholder="Ingrese nombre"
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.containerInput}>
+          <p>Descripcion:</p>
+          <input
+            type="text"
+            name="videogame_description"
+            value={input.videogame_description}
+            placeholder="Ingrese una descripcion"
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.containerInput}>
+          <p>Fecha de lanzamiento: </p>
+          <input
+            type="date"
+            name="videogame_release_date"
+            value={input.videogame_release_date}
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+        </div>
+
+        <div className={styles.containerInput}>
+          <p>Rating:</p>
+          <input
+            type="number"
+            name="videogame_rating"
+            value={input.videogame_rating}
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+        </div>
+
+        <div className={styles.containerInput}>
+          <p>Imagen: </p>
+          <input
+            type="text"
+            name="videogame_image"
+            value={input.videogame_image}
+            disabled={true}
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+        </div>
+        <p className={styles.titleCkeckbox}>Generos: </p>
+        <div className={styles.containerCheckbox}>
+          {genres.map((g, index) => (
+            <div key={index} className={styles.checkbox}>
+              <p className={styles.nameCheck}>{g.genre_name}</p>
+              <input
+                type="checkbox"
+                name="genres"
+                value={g.genre_id}
+                onChange={handleChangeGenre}
+              />
+            </div>
+          ))}
+        </div>
+        <p>Plataformas</p>
+        <div className={styles.containerCheckbox}>
+          {platforms.map((plat, index) => (
+            <div key={index} className={styles.checkbox}>
+              <p className={styles.nameCheck}>{plat.platform_name}</p>
+              <input
+                type="checkbox"
+                name="platforms"
+                value={plat.platform_id}
+                onChange={handleChangePlatform}
+              />
+            </div>
+          ))}
+        </div>
+
+        <input type="submit" value="Guardar" className={styles.button}/>
+        <br />
+      </form>
+      <br /><br />
+    </div>
+  );
+};
+
+export default NewVideogame;
