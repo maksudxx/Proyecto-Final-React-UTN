8059ffbe4f4c7f472e1b35846c76e9f5823c0f52
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-17 11:36:35 -0300

    auth

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,11 +4,12 @@ const router = new Router();
 const videogame = require("./videogame");
 const genre = require("./genre");
 const platform = require("./platform");
+const jwtAuth = require("./jwtAuth");
 const user = require("./user");
 
 router.use("/", videogame);
 router.use("/", genre);
 router.use("/", platform);
-router.use("/auth", user);
-
+router.use("/auth", jwtAuth);
+router.use("/", user);
 module.exports = router;
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,60 +1,19 @@
 const { Router } = require("express");
 const { User } = require("../db");
-const bcrypt = require("bcrypt");
-const { v4: uuidv4 } = require("uuid");
-const jwtGenerator = require("../utils/jwtGenerator");
-const validInfo = require("../middleware/validInfo");
+const authorization = require("../middleware/authorization");
 
 const router = Router();
 
-//registro usuario
-router.post("/register", validInfo, async (req, res, next) => {
-  let { user_name, user_email, user_password } = req.body;
-  try {
-    let user = await User.findOne({
-      where: { user_email: user_email },
-    });
-    if (user) return res.json("el usuario ya existe.!");
+router.get('/user', authorization, async (req, res,next) => {
+    try {
+        //el valor de req.user proviene desde la authorization. esta me sirve para traer la informacion del usuario logueado
+        let user = await User.findOne({
+            where: {user_id : [req.user]}
+        })
+        res.json(user)
+    } catch (error) {
+        next(error);
+    }
+})
 
-    //encriptamos password
-    const salt = 10;
-    const generarSalt = await bcrypt.genSalt(salt);
-    const bcryptPassword = await bcrypt.hash(user_password, salt);
-
-    //creamos el nuevo usuario
-    const newUser = await User.create({
-      user_id: uuidv4(),
-      user_name,
-      user_email,
-      user_password: bcryptPassword,
-    });
-
-    //generamos el JWT password
-    const token = jwtGenerator(newUser.user_id);
-    res.json({ token: token });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post("/login", validInfo, async (req, res, next) => {
-  try {
-    const { user_email, user_password } = req.body;
-    const user = await User.findOne({ where: { user_email: user_email } });
-    if (!user)
-      return res.status(401).json({ message: "password or email invalid" });
-
-    const validPassword = await bcrypt.compare(
-      user_password,
-      user.user_password
-    );
-    if (!validPassword)
-      return res.status(401).json({ message: "password or email invalid" });
-
-    const token = jwtGenerator(user.user_id);
-    res.json({ token: token });
-  } catch (err) {
-    next(err);
-  }
-});
 module.exports = router;
