53eddf44a6b05ce7c15f38f2ec78ef34126f6268
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-05-19 14:49:02 -0300

    refactor

--- client/src/pages/videogameDetails/VideogameDetails.jsx
+++ client/src/pages/videogameDetails/VideogameDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { getVideogameId } from "../../redux/actions/videogameActions";
 import styles from "./VideogameDetails.module.css";
-import Spinner from "../../components/spinner/Spinner";
+import { Loader } from "../../components/loader/Loader";
 
 const VideogameDetails = () => {
   const videogame = useSelector((state) => state.videogame.videogame);
@@ -16,64 +16,70 @@ const VideogameDetails = () => {
   }, [dispatch, videogame_id]);
 
   return (
-    <div className={styles.container}>
-      <h5 className={styles.routes}>/videogames/{videogame.name}</h5>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={styles.container}>
+          <h5 className={styles.routes}>/videogames/{videogame.name}</h5>
 
-      <p className={styles.titleGame}>{videogame.name}</p>
+          <p className={styles.titleGame}>{videogame.name}</p>
 
-      <div className={styles.containerAbout}>
-        <img
-          src={videogame.background_image}
-          alt={videogame.name}
-          className={styles.img}
-        />
+          <div className={styles.containerAbout}>
+            <img
+              src={videogame.background_image}
+              alt={videogame.name}
+              className={styles.img}
+            />
 
-        <div className={styles.containerInfo}>
-          <h3 className={styles.titleAbout}>Acerca de</h3> <br />
-          <p>{videogame.description_raw}</p>
-        </div>
-      </div>
-
-      <div className={styles.containerInfo2}>
-        <div className={styles.div}>
-          <p className={styles.titleInfo}>Fecha de Lanzamiento</p>
-          <p className={styles.info}>{videogame.released}</p>
-        </div>
-        <div className={styles.div}>
-          <p className={styles.titleInfo}>Tags</p>
-          <p className={styles.info}>
-            {videogame.tags?.map((t) => (
-              <p className={styles.nameInfo}>{t.name}</p>
-            ))}
-          </p>
-        </div>
-        <div className={styles.div}>
-          <p className={styles.titleInfo}>Rating</p>
-          <p className={styles.info}>{videogame.rating}</p>
-        </div>
-        <div className={styles.div}>
-          <p className={styles.titleInfo}>Desarrollador</p>
-          {videogame.developers?.map((developer) => (
-            <p className={styles.info}>{developer.name}</p>
-          ))}
-        </div>
-      </div>
-      <div className={styles.containerPlatformAndRequirements}>
-        <br />
-        <h3>
-          <u>Plataformas y Requerimientos</u>
-        </h3>
-        <br />
-        {videogame.platforms?.map((v) => (
-          <div className={styles.requirements}>
-            <li>{v.platform.name}</li>
-            <p>{v.requirements.minimum}</p>
-            <p>{v.requirements.recommended}</p>
+            <div className={styles.containerInfo}>
+              <h3 className={styles.titleAbout}>Acerca de</h3> <br />
+              <p>{videogame.description_raw}</p>
+            </div>
           </div>
-        ))}
-      </div>
-      <br />
-    </div>
+
+          <div className={styles.containerInfo2}>
+            <div className={styles.div}>
+              <p className={styles.titleInfo}>Fecha de Lanzamiento</p>
+              <p className={styles.info}>{videogame.released}</p>
+            </div>
+            <div className={styles.div}>
+              <p className={styles.titleInfo}>Tags</p>
+              <p className={styles.info}>
+                {videogame.tags?.map((t) => (
+                  <p className={styles.nameInfo}>{t.name}</p>
+                ))}
+              </p>
+            </div>
+            <div className={styles.div}>
+              <p className={styles.titleInfo}>Rating</p>
+              <p className={styles.info}>{videogame.rating}</p>
+            </div>
+            <div className={styles.div}>
+              <p className={styles.titleInfo}>Desarrollador</p>
+              {videogame.developers?.map((developer) => (
+                <p className={styles.info}>{developer.name}</p>
+              ))}
+            </div>
+          </div>
+          <div className={styles.containerPlatformAndRequirements}>
+            <br />
+            <h3>
+              <u>Plataformas y Requerimientos</u>
+            </h3>
+            <br />
+            {videogame.platforms?.map((v) => (
+              <div className={styles.requirements}>
+                <li>{v.platform.name}</li>
+                <p>{v.requirements.minimum}</p>
+                <p>{v.requirements.recommended}</p>
+              </div>
+            ))}
+          </div>
+          <br />
+        </div>
+      )}
+    </>
   );
 };
 
