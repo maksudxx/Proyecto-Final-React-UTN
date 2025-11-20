5fe3fdd22852a3918a8e63f61cb434839bd72c0b
Facundo Maksud <facundomaksud@gmail.com>
2025-11-20 01:24:37 -0300

    se movio el index fuera de la carpeta de rutas

---
+++ api/src/index.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const router = new Router();
+const videogame = require("./routes/videogame");
+const genre = require("./routes/genre");
+const platform = require("./routes/platform");
+const jwtAuth = require("./routes/jwtAuth");
+const user = require("./routes/user");
+
+router.use("/", videogame);
+router.use("/", genre);
+router.use("/", platform);
+router.use("/auth", jwtAuth);
+router.use("/", user);
+module.exports = router;
