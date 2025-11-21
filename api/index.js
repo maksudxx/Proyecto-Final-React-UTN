d2276ecd0b8ec62ecb35738786293b5ea9a6c6b7
Facundo Maksud <facundomaksud@gmail.com>
2025-11-21 03:03:12 -0300

    refactor de codigo en el scrpt que activa el servidor

--- api/index.js
+++ api/index.js
@@ -3,14 +3,35 @@ const { conn, Genre, Platform } = require("./src/db.js");
 const { createData } = require("./preloadData");
 require("dotenv").config();
 
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3001, async () => {
-    console.log("%s listening at 3001");
-    const data = await createData();
-    const genre = await Genre.bulkCreate(data.genre);
-    console.log("*********Genres created");
-    const platform = await Platform.bulkCreate(data.platform);
-    console.log("*********Platforms created");
-    console.log("*********Videogames created");
-  });
-});
+const PORT = process.env.PORT || 3001;
+
+async function startServer() {
+  try {
+    // Sincronizar DB
+    await conn.sync({ force: true });
+    console.log("Database synced");
+
+    // Preload de data
+    const { genre, platform } = await createData();
+
+    await Promise.all([
+      Genre.bulkCreate(genre, {
+        ignoreDuplicates: true,
+      }),
+      Platform.bulkCreate(platform, {
+        ignoreDuplicates: true,
+      }),
+    ]);
+
+    console.log("Preload completed (Genres & Platforms created)");
+
+    // Levantar servidor
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error initializing server:", error);
+  }
+}
+
+startServer();
