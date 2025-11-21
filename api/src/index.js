8e31719f2429db9a616f0ddd4da83ba73c283b05
Facundo Maksud <facundomaksud@gmail.com>
2025-11-21 03:05:56 -0300

    refactor de rutas

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 
-const router = Router();
+const router = new Router();
+const videogame = require("./routes/videogame");
+const genre = require("./routes/genre");
+const platform = require("./routes/platform");
+const jwtAuth = require("./routes/jwtAuth");
+const user = require("./routes/user");
 
-router.use("/videogames", require("./routes/videogame"));
-router.use("/genres", require("./routes/genre"));
-router.use("/platforms", require("./routes/platform"));
-router.use("/auth", require("./routes/jwtAuth"));
-router.use("/users", require("./routes/user"));
+router.use("/", videogame);
+router.use("/", genre);
+router.use("/", platform);
+router.use("/auth", jwtAuth);
+router.use("/", user);
 
 module.exports = router;
-
