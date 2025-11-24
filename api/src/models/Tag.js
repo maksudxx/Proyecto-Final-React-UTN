dcfdb6934a356dbc54077a4c81ea10e6aabc308d
Facundo Maksud <facundomaksud@gmail.com>
2025-11-24 16:59:12 -0300

    Model de Tags creado

---
+++ api/src/models/Tag.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("tag", {
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    tag_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+};
