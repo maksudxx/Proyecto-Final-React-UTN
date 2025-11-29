349a9ceaad7546b076f79e54777b3f5dc0520f69
Facundo Maksud <facundomaksud@gmail.com>
2025-11-29 01:38:46 -0300

    Refactor

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,9 +1,9 @@
-import {applyMiddleware, createStore} from 'redux'
-import reducer from '../reducers/index'
+import { applyMiddleware, createStore } from "redux";
+import reducer from "../reducers/index";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-const store = createStore(reducer,  composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-store.subscribe(()=> console.log(store))
+store.subscribe(() => console.log(store));
 
-export default store
+export default store;
