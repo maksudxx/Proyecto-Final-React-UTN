e16088a2d82db8f4f19ef41ef9759f3c16ef328f
Facundo Maksud <facundomaksud@gmail.com>
2025-11-29 01:40:22 -0300

    Refactor

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,15 +1,12 @@
-import {combineReducers} from 'redux'
-import videogameReducer from './videogameReducer';
-import genreReducer from './genreReducer';
-import platformReducer from './platformReducer';
-
-
+import { combineReducers } from "redux";
+import videogameReducer from "./videogameReducer";
+import genreReducer from "./genreReducer";
+import platformReducer from "./platformReducer";
 
 const reducer = combineReducers({
-    videogame: videogameReducer,
-    genre: genreReducer,
-    platform: platformReducer,
-    
-})
+  videogame: videogameReducer,
+  genre: genreReducer,
+  platform: platformReducer,
+});
 
 export default reducer;
