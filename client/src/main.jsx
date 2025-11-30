3bbd2d83204821acc074488e80a03d6f7f2583c6
Facundo Maksud <facundomaksud@gmail.com>
2025-11-30 16:17:20 -0300

    Se renombró el componente pricipal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import store from "./redux/store/index";
+import Videogame from "./Videogame";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router>
       <React.StrictMode>
-        <App />
+        <Videogame />
       </React.StrictMode>
     </Router>
   </Provider>
