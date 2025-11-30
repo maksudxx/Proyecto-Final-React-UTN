d364d022c5e9525bdca607d42d7ee2ab45be6896
Facundo Maksud <facundomaksud@gmail.com>
2025-11-29 21:51:26 -0300

    refactorización de codigo en donde las peticiones son desarrolladas en un custom hook "useVideogameForm

--- client/src/pages/newVideogame/NewVideogame.jsx
+++ client/src/pages/newVideogame/NewVideogame.jsx
@@ -1,62 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { getGenres } from "../../redux/actions/genreActions";
-import { getPlatforms } from "../../redux/actions/platformAction";
-import { useSelector, useDispatch } from "react-redux";
+import { useVideogame } from "../../hooks/useVideogameForm";
 import styles from "./NewVideogame.module.css";
-import { createVideogame } from "../../redux/actions/videogameActions";
-import { useHistory } from "react-router-dom";
-import IMGPRUEBA from "../../assets/prueba_post.jpg";
 
 const NewVideogame = () => {
-  const [input, setInput] = useState({
-    videogame_name: "",
-    videogame_description: "",
-    videogame_release_date: "",
-    videogame_rating: "",
-    videogame_image: IMGPRUEBA,
-    arrayGenres: [],
-    arrayPlatforms: [],
-  });
-
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const genres = useSelector((state) => state.genre.genres);
-  const platforms = useSelector((state) => state.platform.platforms);
-
-  useEffect(() => {
-    dispatch(getGenres());
-    dispatch(getPlatforms());
-  }, [dispatch]);
-
-  const handleInputChange = function (e) {
-    e.preventDefault();
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleCheckboxChange = (e, field) => {
-    const { value, checked } = e.target;
-    setInput((prev) => {
-      const updatedArray = checked
-        ? [...prev[field], value]
-        : prev[field].filter((id) => id !== value);
-      return { ...prev, [field]: updatedArray };
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const result = await dispatch(createVideogame(input));
-    if (result.message === "el juego ya existe en la base de datos") {
-      alert(result.message);
-      return;
-    }
-
-    alert("Videojuego creado..!");
-    history.push("/videogames");
-  };
+  const {
+    handleCheckboxChange,
+    handleInputChange,
+    handleSubmit,
+    platforms,
+    genres,
+    input
+  } = useVideogame();
+  
   return (
     <div className={styles.container}>
       <p className={styles.title}>PUBLICAR UN NUEVO JUEGO</p>
