9dd1a8804ed8aae6525edcee321ee6ebfd44fc2c
Facundo Maksud <facundomaksud@gmail.com>
2025-11-29 21:58:53 -0300

    refactor con custom hook

--- client/src/components/searchbar/SearchBar.jsx
+++ client/src/components/searchbar/SearchBar.jsx
@@ -1,21 +1,13 @@
-import React, { useEffect } from "react";
 import Search from "../search/Search";
-import { useDispatch, useSelector } from "react-redux";
-import { getGenres } from "../../redux/actions/genreActions";
-import { getPlatforms } from "../../redux/actions/platformAction";
 import FilterGenre from "../filter/FilterGenre";
 import FilterPlatforms from "../filter/FilterPlatforms";
-import styles from "./SearchBar.module.css"
+import { useVideogame } from "../../hooks/useVideogameForm";
+import styles from "./SearchBar.module.css";
+
 
 const SearchBar = () => {
-  const dispatch = useDispatch();
-  const genres = useSelector((state) => state.genre.genres);
-  const platforms = useSelector((state) => state.platform.platforms);
-  useEffect(() => {
-    dispatch(getGenres());
-    dispatch(getPlatforms());
-  }, [dispatch]);
 
+  const { genres, platforms } = useVideogame();
   return (
     <div className={styles.container}>
       <FilterGenre props={genres} />
