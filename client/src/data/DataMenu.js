a1b14a269e6f5702b5ff2ca61bc5622dd59fb650
Facundo Maksud <facundomaksud@gmail.com>
2025-12-05 23:21:00 -0300

    Data para el menu

---
+++ client/src/data/DataMenu.js
@@ -0,0 +1,29 @@
+export const options = [
+  { name: "INICIO" },
+  { name: "REVIEWS"},
+  {
+    name: "PLATAFORMAS",
+    data: [
+      "PC",
+      "PlayStation 4",
+      "Xbox One",
+      "Nintendo Switch",
+      "iOS",
+      "Android",
+    ],
+  },
+  {
+    name: "GENEROS",
+    data: [
+      "Free Online Games",
+      "Action",
+      "Strategy",
+      "RPG",
+      "Shooter",
+      "Adventure",
+      "Puzzle",
+      "Racing",
+      "Sports",
+    ],
+  },
+];
