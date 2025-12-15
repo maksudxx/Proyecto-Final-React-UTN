b9e3da8758d27d80ba8136beef5a57fce60be59a
Facundo Maksud <facundomaksud@gmail.com>
2025-12-14 23:21:15 -0300

    Se refactorizo el componente agregando la posibilidad de subir imagenes a la BD y quitar la imagen estatica que se tenia previamente. Ademas se desestructuro los valores de los inputs para una mejor legibilidad

--- client/src/pages/newVideogame/NewVideogame.jsx
+++ client/src/pages/newVideogame/NewVideogame.jsx
@@ -1,50 +1,69 @@
 import SelectForm from "../../components/SelectForm/SelectForm";
+import Spinner from "../../components/spinner/Spinner";
 import { useVideogame } from "../../hooks/useVideogameForm";
 import styles from "./NewVideogame.module.css";
+
+const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+
 const NewVideogame = () => {
   const {
+    handleImageUpload,
     handleInputChange,
     handleSelectChange,
     handleSubmit,
     input,
     genreOptions,
     platformsOptions,
+    loading,
   } = useVideogame();
-  
+
+  const {
+    videogame_name,
+    videogame_description,
+    videogame_rating,
+    videogame_image,
+    videogame_release_date,
+  } = input;
+
   return (
     <div className={styles.container}>
       <p className={styles.title}>PUBLICAR UN NUEVO JUEGO</p>
-      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {/* Nombre del juego */}
         <div className={styles.containerInput}>
           <p>Nombre del juego:</p>
           <input
             type="text"
             name="videogame_name"
-            value={input.videogame_name}
+            value={videogame_name}
             placeholder="Ingrese nombre"
             onChange={handleInputChange}
             className={styles.input}
-            required="true"
+            required
           />
         </div>
+
+        {/* Descripción */}
         <div className={styles.containerInput}>
-          <p>Descripcion:</p>
+          <p>Descripción:</p>
           <input
             type="text"
             name="videogame_description"
-            value={input.videogame_description}
-            placeholder="Ingrese una descripcion"
+            value={videogame_description}
+            placeholder="Ingrese una descripción"
             onChange={handleInputChange}
             className={styles.input}
-            required="true"
+            required
           />
         </div>
+
+        {/* Fecha de lanzamiento */}
         <div className={styles.containerInput}>
-          <p>Fecha de lanzamiento: </p>
+          <p>Fecha de lanzamiento:</p>
           <input
             type="date"
             name="videogame_release_date"
-            value={input.videogame_release_date}
+            value={videogame_release_date}
             onChange={handleInputChange}
             onFocus={(e) => e.target.showPicker?.()}
             className={styles.input}
@@ -52,6 +71,7 @@ const NewVideogame = () => {
           />
         </div>
 
+        {/* Rating */}
         <div className={styles.containerInput}>
           <p>Rating:</p>
           <input
@@ -59,40 +79,62 @@ const NewVideogame = () => {
             min={1}
             max={5}
             name="videogame_rating"
-            value={input.videogame_rating}
+            value={videogame_rating}
             onChange={handleInputChange}
             className={styles.input}
-            required="true"
             placeholder="del 1 al 5"
+            required
           />
         </div>
 
-        <div className={styles.containerInput}>
-          <p>Imagen: </p>
+        {/* Imagen principal */}
+        <div className={`${styles.containerInput} ${styles.containerImage}`}>
+          <p>Imagen Principal:</p>
           <input
-            type="text"
+            type="file"
             name="videogame_image"
-            value={input.videogame_image}
-            disabled={true}
-            onChange={handleInputChange}
-            className={styles.input}
+            onChange={handleImageUpload}
+            className={styles.inputFile}
+            disabled={loading}
           />
+
+          {loading && (
+            <div className={styles.spinnerContainer}>
+              <Spinner />
+              <span>Subiendo imagen...</span>
+            </div>
+          )}
+
+          {!loading && videogame_image && (
+            <img
+              src={
+                videogame_image.startsWith("http")
+                  ? videogame_image
+                  : `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/${videogame_image}`
+              }
+              alt="Preview"
+              className={styles.imgPreview}
+            />
+          )}
         </div>
 
+        {/* Select de géneros */}
         <SelectForm
-          title={"Generos"}
+          title="Géneros"
           option={genreOptions}
           field="arrayGenres"
           handleSelectChange={handleSelectChange}
         />
-        
+
+        {/* Select de plataformas */}
         <SelectForm
-          title={"Plataformas"}
+          title="Plataformas"
           option={platformsOptions}
           field="arrayPlatforms"
           handleSelectChange={handleSelectChange}
         />
 
+        {/* Botón enviar */}
         <input type="submit" value="Guardar" className={styles.button} />
       </form>
     </div>
