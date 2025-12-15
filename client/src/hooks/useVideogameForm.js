32f56b4a3eb86518d10a4ad2f408a61fd57bd4e4
Facundo Maksud <facundomaksud@gmail.com>
2025-12-14 23:20:03 -0300

    se uso el custom hook para subir la imagen y retornar la url de la imagen para insertar en la bd

--- client/src/hooks/useVideogameForm.js
+++ client/src/hooks/useVideogameForm.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { getGenres } from "../redux/actions/genreActions";
 import { getPlatforms } from "../redux/actions/platformAction";
 import { createVideogame } from "../redux/actions/videogameActions";
-import IMGPRUEBA from "../../../client/src/assets/prueba_post.jpg";
+import { useCloudinaryUpload } from "./useCloudinary";
 
 export const useVideogame = () => {
   const [input, setInput] = useState({
@@ -12,18 +12,18 @@ export const useVideogame = () => {
     videogame_description: "",
     videogame_release_date: "",
     videogame_rating: "",
-    videogame_image: IMGPRUEBA,
+    videogame_image: "",
     arrayGenres: [],
     arrayPlatforms: [],
     arrayTags: [],
     arrayDevelopers: [],
   });
 
+  const { upload, loading } = useCloudinaryUpload();
   const dispatch = useDispatch();
   const history = useHistory();
   const genres = useSelector((state) => state.genre.genres);
   const platforms = useSelector((state) => state.platform.platforms);
-
   useEffect(() => {
     dispatch(getGenres());
     dispatch(getPlatforms());
@@ -49,10 +49,26 @@ export const useVideogame = () => {
     label: g.genre_name,
   }));
 
-   const platformsOptions = platforms.map((p) => ({
+  const platformsOptions = platforms.map((p) => ({
     value: p.platform_id,
     label: p.platform_name,
   }));
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    console.log("Archivo seleccionado:", file);
+    if (!file) return;
+
+    try {
+      const result = await upload(file);
+      setInput((prev) => ({
+        ...prev,
+        videogame_image: result.secure_url,
+      }));
+    } catch (err) {
+      console.error("Error subiendo la imagen:", err);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -67,11 +83,13 @@ export const useVideogame = () => {
   };
 
   return {
+    handleImageUpload,
     handleInputChange,
     handleSelectChange,
     handleSubmit,
     genreOptions,
     platformsOptions,
     input,
+    loading
   };
 };
