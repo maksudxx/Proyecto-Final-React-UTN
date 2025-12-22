a045ea4f8d1180f1a81f127fc5ee502ddf152773
Facundo Maksud <facundomaksud@gmail.com>
2025-12-21 22:35:10 -0300

    mejora en el store

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -4,6 +4,6 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-store.subscribe(() => console.log(store));
+store.subscribe(() => console.log("Estado actualizado:", store.getState()));
 
 export default store;
