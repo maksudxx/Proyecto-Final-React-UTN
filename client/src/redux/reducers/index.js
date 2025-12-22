9e6cbc88fc649b5a68d97dd76b1980a348ff81b8
Facundo Maksud <facundomaksud@gmail.com>
2025-12-21 22:34:42 -0300

    se agrega el tagReducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import videogameReducer from "./videogameReducer";
 import genreReducer from "./genreReducer";
 import platformReducer from "./platformReducer";
+import TagReducer from "./TagReducer";
 
 const reducer = combineReducers({
   videogame: videogameReducer,
   genre: genreReducer,
   platform: platformReducer,
+  tag: TagReducer
 });
 
 export default reducer;
