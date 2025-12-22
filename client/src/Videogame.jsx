18856ada20ee3701b6a5fdb896d9dc3e6c871f31
Facundo Maksud <facundomaksud@gmail.com>
2025-12-22 15:21:57 -0300

    se quito la funcion de autenticacion y se hace llamado a traves de un custom hook. se realizo la refactorizacion de codigo en donde se llama un componente que envuelve las rutas privadas y ayudan que al hacer F5 no me redirija a la raiz.

--- client/src/Videogame.jsx
+++ client/src/Videogame.jsx
@@ -1,5 +1,4 @@
 import { Switch, Route, Redirect } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Videogames from "./pages/videogames/Videogames";
 import Header from "./components/header/Header";
 import VideogameDetails from "./pages/videogameDetails/VideogameDetails";
@@ -9,81 +8,44 @@ import Footer from "./components/footer/Footer";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Sesion from "./components/Sesion/Sesion";
+import { useAuth } from "./hooks/useAuth";
+import { PrivateRoute } from "./components/privateRoute/PrivateRoute";
 
 function Videogame() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { isAuthenticated, setAuth, checkAuth } = useAuth();
 
-  const setAuth = (boolean) => {
-    setIsAuthenticated(boolean);
-  };
-
-  async function isAuth() {
-    try {
-      const response = await fetch("http://localhost:3001/auth/is-verify", {
-        method: "GET",
-        headers: { token: localStorage.token },
-      });
-
-      const parseRes = await response.json();
-      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-  useEffect(() => {
-    isAuth();
-  }, []);
   return (
     <div className="App">
+      <Header setAuth={setAuth} isAuthenticated={isAuthenticated} />
+      <Sesion setAuth={setAuth} isAuthenticated={isAuthenticated} />
+
       <Switch>
-        <Route
-          exact
-          path="/login"
-          render={(props) =>
-            !isAuthenticated ? (
-              <Login {...props} setAuth={setAuth} />
-            ) : (
-              <Redirect to="/" />
-            )
-          }
-        />
-        <Route
-          exact
-          path="/register"
-          render={(props) =>
-            !isAuthenticated ? (
-              <Register {...props} setAuth={setAuth} />
-            ) : (
-              <Redirect to="/login" />
-            )
-          }
-        />
-        <Route path="/">
-          <Header setAuth={setAuth} isAuthenticated={isAuthenticated} />
-          <Sesion setAuth={setAuth} isAuthenticated={isAuthenticated} />
-          <Route exact path="/" component={Videogames} />
-          <Route
-            exact
-            path="/videogames/:videogame_id"
-            render={(props) => (
-              <VideogameDetails {...props} isAuthenticated={isAuthenticated} />
-            )}
-          />
-          <Route
-            exact
-            path="/newGame"
-            render={(props) =>
-              isAuthenticated ? (
-                <NewVideogame {...props} />
-              ) : (
-                <Redirect to="/login" />
-              )
-            }
-          />
-          <Route exact path="/about" component={About} />
-          <Footer />
+        {/* Rutas públicas */}
+        <Route exact path="/login">
+          {!isAuthenticated ? <Login setAuth={setAuth} /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/register">
+          {!isAuthenticated ? (
+            <Register setAuth={setAuth} />
+          ) : (
+            <Redirect to="/" />
+          )}
+        </Route>
+        <Route exact path="/" component={Videogames} />
+        <Route exact path="/videogames/:videogame_id">
+          <VideogameDetails isAuthenticated={isAuthenticated} />
+        </Route>
+        <Route exact path="/about" component={About} />
+
+        {/* Rutas privadas */}
+        <Route exact path="/newGame">
+          <PrivateRoute isAuthenticated={isAuthenticated} checkAuth={checkAuth}>
+            <NewVideogame />
+          </PrivateRoute>
         </Route>
       </Switch>
+
+      <Footer />
     </div>
   );
 }
