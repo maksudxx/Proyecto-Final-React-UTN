317324c563811670d5439513ce6048d85c701def
Facundo Maksud <facundomaksud@gmail.com>
2025-12-25 17:15:03 -0300

    Se refactorizo el codigo, el nombre y el estado de la sesion ahora se manejan desde el action de redux

--- client/src/components/sesion/Sesion.jsx
+++ client/src/components/sesion/Sesion.jsx
@@ -1,45 +1,23 @@
-import React,{ useState, useEffect} from 'react'
-import styles from './Sesion.module.css'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector, useDispatch } from "react-redux";
+import { logoutAction } from "../../redux/actions/authActions";
+import styles from "./Sesion.module.css";
+export const Sesion = () => {
+  const dispatch = useDispatch();
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
 
-const Sesion = ({setAuth, isAuthenticated}) => {
-    const [name, setName] = useState("");
-    const history = useHistory()
-    async function getName() {
-      try {
-        const response = await fetch("http://localhost:3001/user", {
-          method: "GET",
-          headers: { token: localStorage.token },
-        });
-        const parseRes = await response.json();
-        setName(parseRes.user_name);
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-    useEffect(() => {
-      getName();
-    }, []);
-  
-    const logout = (e) => {
-      e.preventDefault();
-      localStorage.removeItem("token");
-      setAuth(false);
-      history.push("/")
-    };
+  if (!isAuthenticated || !user) return null;
+  console.log(user)
   return (
-      
     <div className={styles.container}>
-        {isAuthenticated === true ? (
-        <span className={styles.containerLogin}>
-          <span>Bienvenido {name}</span>
-          <button onClick={logout} className={styles.buttonLogout}>
-            Cerrar sesion
-          </button>
-        </span>
-      ) : null}
+      <span className={styles.containerLogin}>
+        <span>Bienvenido {user.user_name}</span>
+        <button
+          onClick={() => dispatch(logoutAction())}
+          className={styles.buttonLogout}
+        >
+          Cerrar sesion
+        </button>
+      </span>
     </div>
-  )
-}
-
-export default Sesion
+  );
+};
