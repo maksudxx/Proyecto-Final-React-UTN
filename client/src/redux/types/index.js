0450c370196d54d86007292b57848b86442340e9
Facundo Maksud <facundomaksud@gmail.com>
2025-12-25 17:09:41 -0300

    Se agrego los types para login

--- client/src/redux/types/index.js
+++ client/src/redux/types/index.js
@@ -20,5 +20,11 @@ export const GET_USER = "GET_USER"
 //Tags
 export const GET_TAG = "GET_TAG"
 
+//Auth
+export const AUTH_LOADING = "AUTH_LOADING";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGOUT = "LOGOUT";
+
 //Developers
 
