e867cfdefb877ddf9398ff9e4a32b04cab6b7881
Facundo Maksud <facundomaksud@gmail.com>
2025-12-25 17:10:15 -0300

    Se combino en los reducers el inicio de sesion

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import videogameReducer from "./videogameReducer";
 import genreReducer from "./genreReducer";
 import platformReducer from "./platformReducer";
 import TagReducer from "./TagReducer";
+import authReducer from "./authReducer";
 
 const reducer = combineReducers({
   videogame: videogameReducer,
   genre: genreReducer,
   platform: platformReducer,
-  tag: TagReducer
+  tag: TagReducer,
+  auth: authReducer
 });
 
 export default reducer;
