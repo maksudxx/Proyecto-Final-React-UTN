13e82691182c2a1220f6b1b3c36735230f4e0c51
Facundo Maksud <facundomaksud@gmail.com>
2025-12-26 00:35:59 -0300

    Se agrego toast de confirmación

--- client/src/components/sesion/Sesion.jsx
+++ client/src/components/sesion/Sesion.jsx
@@ -1,18 +1,23 @@
 import { useSelector, useDispatch } from "react-redux";
 import { logoutAction } from "../../redux/actions/authActions";
 import styles from "./Sesion.module.css";
+import { confirmToast } from "../../utils/confirmToast";
 export const Sesion = () => {
   const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector((state) => state.auth);
 
   if (!isAuthenticated || !user) return null;
-  console.log(user)
   return (
     <div className={styles.container}>
       <span className={styles.containerLogin}>
         <span>Bienvenido {user.user_name}</span>
         <button
-          onClick={() => dispatch(logoutAction())}
+          onClick={() =>
+            confirmToast({
+              text: "¿Desea cerrar esta sesión?",
+              onConfirm: () => dispatch(logoutAction()),
+            })
+          }
           className={styles.buttonLogout}
         >
           Cerrar sesion
