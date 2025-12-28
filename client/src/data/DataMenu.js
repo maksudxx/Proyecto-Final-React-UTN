ad9eedb1110f8184e9b021c5a461463a9b91fb72
Facundo Maksud <facundomaksud@gmail.com>
2025-12-28 18:03:13 -0300

    se hizo cambios para la obtencion de data

--- client/src/data/DataMenu.js
+++ client/src/data/DataMenu.js
@@ -3,32 +3,30 @@ import { BsNintendoSwitch } from "react-icons/bs";
 import { MdOutlinePhoneIphone } from "react-icons/md";
 import { TfiAndroid } from "react-icons/tfi";
 
-export const options = [
-  { name: "INICIO" },
-  { name: "REVIEWS" },
-  {
-    name: "PLATAFORMAS",
-    data: [
-      { name: "PC", icon: FaDesktop },
-      { name: "PlayStation 4", icon: FaPlaystation },
-      { name: "Xbox One", icon: FaXbox },
-      { name: "Nintendo Switch", icon: BsNintendoSwitch },
-      { name: "iOS", icon: MdOutlinePhoneIphone },
-      { name: "Android", icon: TfiAndroid },
-    ],
-  },
-  {
-    name: "GENEROS",
-    data: [
-      { name: "Free Online Games", icon: FaGamepad },
-      { name: "Action", icon: FaGamepad },
-      { name: "Strategy", icon: FaGamepad },
-      { name: "RPG", icon: FaGamepad },
-      { name: "Shooter", icon: FaGamepad },
-      { name: "Adventure", icon: FaGamepad },
-      { name: "Puzzle", icon: FaGamepad },
-      { name: "Racing", icon: FaGamepad },
-      { name: "Sports", icon: FaGamepad },
-    ],
-  },
-];
+export const platformsList = {
+  name: "PLATAFORMAS",
+  path: "/platforms",
+  data: [
+    { name: "PC", icon: FaDesktop },
+    { name: "PlayStation 4", icon: FaPlaystation },
+    { name: "Xbox One", icon: FaXbox },
+    { name: "Nintendo Switch", icon: BsNintendoSwitch },
+    { name: "iOS", icon: MdOutlinePhoneIphone },
+    { name: "Android", icon: TfiAndroid },
+  ],
+};
+export const genresList = {
+  name: "GENEROS",
+  path: "/genres",
+  data: [
+    { name: "Free Online Games", icon: FaGamepad },
+    { name: "Action", icon: FaGamepad },
+    { name: "Strategy", icon: FaGamepad },
+    { name: "RPG", icon: FaGamepad },
+    { name: "Shooter", icon: FaGamepad },
+    { name: "Adventure", icon: FaGamepad },
+    { name: "Puzzle", icon: FaGamepad },
+    { name: "Racing", icon: FaGamepad },
+    { name: "Sports", icon: FaGamepad },
+  ],
+};
