1a00c95f7ca8d6bbb6ec73784f5ae2a1f36cc8c0
Facundo Maksud <facundomaksud@gmail.com>
2025-12-28 18:03:36 -0300

    Se dio eventos de click a las opciones del menu para los generos y plataformas

---
+++ client/src/components/MenuAside/MenuAside.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { genresList, platformsList } from "../../data/DataMenu";
+import styles from "./MenuAside.module.css";
+import { useDispatch } from "react-redux";
+import {
+  filterVideogamesGenre,
+  filterVideogamesPlatform,
+} from "../../redux/actions/videogameActions";
+
+export const MenuAside = () => {
+  const [genre, setGenre] = useState("");
+  const [platform, setPlatform] = useState("");
+  const dispatch = useDispatch();
+
+  const searchGenre = (name) => {
+    setGenre(name);
+    dispatch(filterVideogamesGenre(name));
+  };
+
+  const searchPlatform = (name) => {
+    setPlatform(name);
+    dispatch(filterVideogamesPlatform(name));
+  };
+
+  const { data: datagenres } = genresList;
+  const { data: dataPlatforms } = platformsList;
+
+  return (
+    <aside className={styles.menuContainer}>
+      <div>
+        <p className={styles.title}>INICIO</p>
+      </div>
+      <div>
+        <p className={styles.title}>REVIEWS</p>
+      </div>
+      <div>
+        <p className={styles.title}>GENEROS</p>
+        {datagenres.map(({ name, icon: Icon }) => (
+          <div key={name} className={styles.item} onClick={() => searchGenre(name)}>
+            <p className={styles.option}>
+              <span>
+                <Icon />
+              </span>{" "}
+              {name}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p className={styles.title}>PLATAFORMAS</p>
+        {dataPlatforms.map(({ name, icon: Icon }) => (
+          <div key={name} className={styles.item} onClick={()=> searchPlatform(name)}>
+            <p className={styles.option}>
+              <span>
+                <Icon />
+              </span>{" "}
+              {name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </aside>
+  );
+};
