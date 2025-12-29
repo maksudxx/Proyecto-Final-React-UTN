29ee20ae0edb75e0a90ff411696b90e290d8e206
Facundo Maksud <facundomaksud@gmail.com>
2025-12-28 21:26:21 -0300

    Refactor

--- client/src/components/spinner/Spinner.jsx
+++ client/src/components/spinner/Spinner.jsx
@@ -1,12 +1,9 @@
-import React from "react";
 import { FaSpinner } from "react-icons/fa";
 import styles from "./Spinner.module.css";
-const Spinner = () => {
+export const Spinner = () => {
   return (
     <div className={styles.spinner}>
       <FaSpinner className={styles.spinning} size={60} />
     </div>
   );
 };
-
-export default Spinner;
