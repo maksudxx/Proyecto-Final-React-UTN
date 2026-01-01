a786ffc392dffa4e34c4f1da1f1ba5a3d252ec50
Facundo Maksud <facundomaksud@gmail.com>
2026-01-01 20:40:12 -0300

    se quitaron importaciones que se se usan dentro de los componentes

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Register.module.css";
 import { useDispatch } from "react-redux";
